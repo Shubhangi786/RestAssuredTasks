ffb19a50cfb54162db852b4c5f40b3db16b244d6
Shubhangi Sable <shubhangimadhukar_sable@epam.com>
2024-06-10 18:52:05 +0530

    Indented code

--- src/main/java/Task4/ValidateWeatherAPI.java
+++ src/main/java/Task4/ValidateWeatherAPI.java
@@ -17,46 +17,31 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 
 public class ValidateWeatherAPI {
-	
+
 	static String apiKey;
-	
-	public static void main(String [] args) {
+
+	public static void main(String[] args) {
 		RestAssured.baseURI = "http://api.openweathermap.org/data/2.5/weather";
-		
+
 		Properties prop = new Properties();
 		try {
 			prop.load(new FileInputStream("src\\main\\java\\Task4\\config.properties"));
 			apiKey = (String) prop.get("appid");
 		} catch (Exception e) {
 			System.out.println(e);
-		} 
-		
-		
-		Response response1 = given()
-								.accept(ContentType.JSON)
-								.queryParam("q", "hyderabad")
-								.queryParam("appid", apiKey)
-					       .when()
-					       		.get();
-			
+		}
+
+		Response response1 = given().accept(ContentType.JSON).queryParam("q", "hyderabad").queryParam("appid", apiKey)
+				.when().get();
+
 		String lattitude = response1.jsonPath().getString("coord.lat");
 		String longitude = response1.jsonPath().getString("coord.lon");
-		
-		Response response2 = given()
-								.accept(ContentType.JSON)
-								.queryParam("lat", lattitude)
-								.queryParam("lon", longitude)
-								.queryParam("appid", apiKey)
-							.when()
-									.get();
-	
-		response2.then().log().all()
-					.body("name", equalTo("Hyderabad"))
-					.body("sys.country", equalTo("IN"))
-					.body("main.temp_min", greaterThan(0F))
-					.body("main.temp", greaterThan(0F));
-		
-               
+
+		Response response2 = given().accept(ContentType.JSON).queryParam("lat", lattitude).queryParam("lon", longitude)
+				.queryParam("appid", apiKey).when().get();
+
+		response2.then().log().all().body("name", equalTo("Hyderabad")).body("sys.country", equalTo("IN"))
+				.body("main.temp_min", greaterThan(0F)).body("main.temp", greaterThan(0F));
+
 	}
 }
-	
